3cad9d6b3f3929906eefe311e65a49d69c5ca561
Yuriy Artamonov <jreznot@yandex.ru>
2017-08-30 14:57:52 +0400

    Ignore unnecessary resources during electron packaging #6

--- electron-app-package.bat
+++ electron-app-package.bat
@@ -1,6 +1,6 @@
 call gradlew bundle
 rmdir .\electron-src\electron-vaadin-win32-x64\ /s /q
 cd electron-src
-call electron-packager . --icon=icon.ico
+call electron-packager . --icon=icon.ico --ignore=README.md --ignore=.npmignore --ignore=.travis.yml
 xcopy ..\build\install\electron-vaadin .\electron-vaadin-win32-x64\electron-vaadin\ /E
 cd ..
